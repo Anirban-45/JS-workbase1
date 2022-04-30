be7c3d7eac7beb37bcd16122f174af75216ba557
Anirban-45 <azadarannya@gmail.com>
2022-05-01 03:42:11 +0600

    Newly opened Workbase

---
+++ index.js
@@ -0,0 +1,15 @@
+//alert("stoopid Alert");
+
+//var age = 21;
+//var name = "Anniline";
+//var status = true;
+
+//console.log(name+ "'s age is "+ age);
+//console.log("status:",status);
+
+//var name = ;
+
+document.getElementById("myButton").onclick = function() {
+
+    var myName = document.getElementById('id');
+}

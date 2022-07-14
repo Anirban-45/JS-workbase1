38a7fcbc56496cde9dbdf0208d350b58cf3061b3
Anirban-45 <azadarannya@gmail.com>
2022-07-14 15:36:43 +0600

    getElementByID can change in DOM

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ document.getElementById("myButton").onclick = function(){
 
     var myName = document.getElementById("myText").value;
     console.log(myName);
+
+    document.getElementById("username").innerHTML = myName;
 }

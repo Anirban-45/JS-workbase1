39c0606eed1ac440b2106b8d579dac4de1e6cd61
Anirban-45 <azadarannya@gmail.com>
2022-07-15 03:16:46 +0600

    Math functions

---
+++ math.js
@@ -0,0 +1,19 @@
+let ans, maximum, minimum;
+let x = 2.73;
+let y = 7;
+let z = 1.5;
+
+//ans = Math.round(x);
+//ans = Math.floor(x);
+//ans = Math.ceil(x);
+//ans = Math.pow(6, x);
+//ans = Math.sqrt(x);
+//ans = Math.abs(x);
+ans = Math.PI;
+
+maximum = Math.max(x, y, z);
+minimum = Math.min(x, y, z);
+
+console.log(ans);
+console.log(maximum);
+console.log(minimum);

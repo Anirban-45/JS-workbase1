71d62d10d700c003b243fe68b9af31e716b979b2
Anirban-45 <azadarannya@gmail.com>
2022-07-14 17:00:19 +0600

    Type conversions

--- variables.js
+++ variables.js
@@ -12,3 +12,24 @@ console.log("status:",status);
 //Showing on browser or DOM
 
 document.getElementById("p2").innerHTML = "Your are " + age + "years old."
+
+// Type conversion
+
+let ageX = window.prompt("How old were you last year?"); //string
+console.log(typeof ageX); // finds out the datatype
+
+ageX = Number(ageX); // Number() changes other datatypes to number
+console.log(typeof ageX);
+ageX++;
+console.log("Happy birthday! You're", ageX, "years old!");
+
+let x = Number("1.456");
+let y = String(3.256);
+let z = Boolean("sum"); // true as there is input
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+z = Boolean("");
+console.log(z, typeof z); // false

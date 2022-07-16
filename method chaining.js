4f961b4313f767e7477a0d8cb125972485c6d036
Anirban-45 <azadarannya@gmail.com>
2022-07-16 10:01:09 +0600

    Method chaining

---
+++ method chaining.js
@@ -0,0 +1,6 @@
+// we can chain methods by continuously using the . keyword
+
+let username = "Suzanne";
+
+let letter = username.charAt(4).toUpperCase(); // here by . we can add methods to help us
+console.log(letter);

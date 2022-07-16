5ec5768da2766bc560cf5251a19756a1b3d23df4
Anirban-45 <azadarannya@gmail.com>
2022-07-16 10:21:18 +0600

    if else statement

---
+++ if else.js
@@ -0,0 +1,17 @@
+// if statement - basic form of decision making.
+
+let age = 21;
+
+
+if (age > 50) {
+    console.log("You are old");
+}
+else if (age < 0) {
+    console.log("You havent been born yet");
+}
+else if (age >= 18) {
+    console.log("You are an adult!");
+}
+else {
+    console.log("You are not an adult!");
+}
